025ff4e3b168264a891e951cf5960948e16b8af2
Aditya9119 <adityakumar9119@gmail.com>
2023-10-31 09:40:08 +0530

    final

---
+++ ecommerce-admin/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:[
+            "res.cloudinary.com"
+        ]
+    }
+}
+
+module.exports = nextConfig
+
+
+
+// 4.11.00 
